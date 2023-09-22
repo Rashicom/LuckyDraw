a5fa3320f2812266909a2582a1b8e06c36852aa9
rashid kp <rashid.kp484@gmail.com>
2023-09-23 02:50:13 +0530

    coupen validator class completed, Testing requiered

--- luckydraw/coupens.py
+++ luckydraw/coupens.py
@@ -1,5 +1,5 @@
 from .models import LuckyDraw
-
+import re
 
 
 # coupen validation check agains its type
@@ -16,6 +16,10 @@ class CoupenValidator:
         """
         calling different validtin fuction accorting to the coupen type
         """
+        # if contain specia charecter return false
+        if self.is_contain_special_char():
+            return False
+
         if self.coupen_type == "BOX":
             return self.is_box_coupen()
         
@@ -27,8 +31,27 @@ class CoupenValidator:
         """
         this methods returns True if the coupen is a valied box coupen else return False
         """
-        pass
-    
+        
+        # seperate integer to one string, alphabet to another string for checking
+        number = ""
+        char = ""
+        for i in self.coupen_number:
+            if i.isnumeric():
+                number = number+i
+            else:
+                char = char+i
+
+        
+        if len(number) == 2 and len(char) == 3:
+            return False
+
+        # bothe number and char leng must be below 3 and not be zero
+        if 0 < len(number) <= 3 or 0 < len(char) <= 3:
+            return True
+
+        else:
+            return False
+            
 
     def is_block_or_super(self):
         """
@@ -37,6 +60,20 @@ class CoupenValidator:
         """
 
         if self.coupen_number.isnumeric() and len(self.coupen_number) == 3:
+            return True
+        else:
+            return False
+        
+    
+    def is_contain_special_char(self):
+        """
+        checking the coupen contain any special charecter or not
+        if contains returns true 
+        """
+
+        regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+
+        if regex.search(self.coupen_number) is not None:
             return True
         else:
             return False
