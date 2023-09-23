0205ff3c3001a535139c0f35532e623885be96ac
rashid kp <rashid.kp484@gmail.com>
2023-09-23 14:22:12 +0530

    test case implimented for coupen validator and tested OK

--- luckydraw/tests.py
+++ luckydraw/tests.py
@@ -7,28 +7,58 @@ from .coupens import CoupenValidator
 
 class CoupenValidatorTest(TestCase):
 
-    def test_block_valied_coupen(self):
-        valied_coupens = ["123","111","121","122"]
+    # valied coupens
+    block_valied_coupens = ["123","111","121","122"]
+    super_valied_coupens = ["123","111","121","122"]
+    box_valied_coupens = ["ABC1","abc1","aBc1","A1","AB1","AB23","aB24","CA1"]
 
-        for coupen in valied_coupens:
+    # invalied coupens
+    block_invalied_coupens = ["12","1115","","5","AB1","a"]
+    super_invalied_coupens = ["12","1115","","5","AB1","a"]
+    box_invalied_coupens = ["ABC","abc","121","A12","AB123","ABCD","AD1","","AB_1","ABA1","AB3455"]
+
+    # valied coupen checking
+    def test_block_valied_coupen(self):
+
+        for coupen in self.block_valied_coupens:
             validator = CoupenValidator(coupen_number=coupen, coupen_type="BLOCK")
-            self.assertTrue(validator.is_valied())
+            self.assertTrue(validator.is_valied(), f"Validation failed for coupen: {coupen}")
 
 
     def test_super_valied_coupen(self):
-        valied_coupens = ["123","111","121","122"]
-
-        for coupen in valied_coupens:
+        
+        for coupen in self.super_valied_coupens:
             validator = CoupenValidator(coupen_number=coupen, coupen_type="BLOCK")
-            self.assertTrue(validator.is_valied())
+            self.assertTrue(validator.is_valied(), f"Validation failed for coupen: {coupen}")
     
+
 
     def test_box_valied_coupens(self):
-        valied_coupens = ["ABC1","abc1","aBc1","A1","AB1","AB23","aB24","CA1"]
 
-        for coupen in valied_coupens:
+        for coupen in self.box_valied_coupens:
             validator = CoupenValidator(coupen_number=coupen, coupen_type="BOX")
-            self.assertTrue(validator.is_valied())
+            self.assertTrue(validator.is_valied(), f"Validation failed for coupen: {coupen}")
 
     
+    
+    # invalied coupen checking
+    def test_block_invalid_coupens(self):
+
+        for coupen in self.block_invalied_coupens:
+            validator = CoupenValidator(coupen_number=coupen, coupen_type="BLOCK")
+            self.assertFalse(validator.is_valied(), f"Validation failed for coupen: {coupen}")
+
+    
+    def test_super_invalid_coupens(self):
+
+        for coupen in self.super_invalied_coupens:
+            validator = CoupenValidator(coupen_number=coupen, coupen_type="SUPER")
+            self.assertFalse(validator.is_valied(), f"Validation failed for coupen: {coupen}")
+    
+
+    def test_box_invalid_coupens(self):
+
+        for coupen in self.box_invalied_coupens:
+            validator = CoupenValidator(coupen_number=coupen, coupen_type="BOX")
+            self.assertFalse(validator.is_valied(), f"Validation failed for coupen: {coupen}")
     
