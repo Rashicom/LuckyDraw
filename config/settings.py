48c50f6213f6d23142dbdf13ab97651861dbb3ec
Rashicom <rashidkp.484@gmail.com>
2024-03-21 21:09:19 +0530

    chache file distroyed

--- config/settings.py
+++ config/settings.py
@@ -87,17 +87,27 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "postgres",
+#         "USER": "postgres",
+#         "PASSWORD": "123",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
+        "NAME": "luckydrow_db",
         "USER": "postgres",
         "PASSWORD": "123",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- luckydraw/urls.py
+++ luckydraw/urls.py
@@ -10,9 +10,10 @@ urlpatterns = [
     path('results/', views.Results.as_view(),name="results"),
     path('user_report/',views.UserReport.as_view(),name="user_report"),
     path('additional_billing/', views.AdditionalBillingReport.as_view(),name="additional_billing"),
+
+    # pdf generatin end points
     path('user_report_pdf/', views.UserReportPdf.as_view(), name="user_report_pdf"),
     path('winners_report_pdf/', views.WinnerAnnouncementPdf.as_view(), name="winners_report_pdf"),
     path('result_filter_pdf/', views.ResultFilterPdf.as_view(),name="result_filter_pdf"),
     path('additional_billing_pdf/', views.AdditionalBillingPdf.as_view(),name="additional_billing_pdf"),
-    
 ]
